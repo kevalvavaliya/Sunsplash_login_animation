de5097e95c91a7a93c2926affdc41040818ded76
keval vavaliya <kevalvavaliya@gmail.com>
2021-02-06 16:22:20 +0530

    first commit

---
+++ app/src/main/java/com/example/splashlogin/Activity_splash_login.java
@@ -0,0 +1,14 @@
+package com.example.splashlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_splash_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+    }
+}
